a03701797a81eae9f80fedf02ed9196f1b763e3d
Dmitry Kovalev <kov.dmm@gmail.com>
2026-04-16 02:15:05 +0500

    fix ERR_MODULE_NOT_FOUND

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/sf.ts", "src/executors.ts"],
+    entry: ["src/index.ts"],
     format: ["esm"],
     dts: true,
     sourcemap: true,
